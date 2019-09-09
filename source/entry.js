0156a1b6b8570f90643ab2caa51011789164b9a9
Daniel Santos <daniel.santos@cst.org>
2019-09-09 11:18:29 -0400

    Add Prettier formatting (local instead of global)

--- source/entry.js
+++ source/entry.js
@@ -1,18 +1,18 @@
-const API_URL = 'https://randomuser.me/api/?results=3';
+const API_URL = "https://randomuser.me/api/?results=3";
 
 const results = fetch(API_URL)
-  .then((x) => {
-    console.info('x', x);
+  .then(x => {
+    console.info("x", x);
     // Stream is converted to Promise after json() call
-    return x.json().then((y) => {
+    return x.json().then(y => {
       const results = y.results;
-      console.info('results', results);
-      results.map((z) => {
-        console.log('name:', z.name.first + ' ' + z.name.last);
+      console.info("results", results);
+      results.map(z => {
+        console.log("name:", z.name.first + " " + z.name.last);
         return z.picture.medium;
       });
     });
   })
-  .catch((e) => console.error(e));
+  .catch(e => console.error(e));
 
-console.log('Here we go!!!', results);
+console.log("Here we go!!!", results);
