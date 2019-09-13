e06fdd786afe2dcd86592294a802f4090bd8c433
Daniel Santos <daniel@brasileiro.ca>
2019-09-13 15:17:23 -0400

    Update to list WP-API routes

--- source/entry.js
+++ source/entry.js
@@ -1,18 +1,16 @@
-const API_URL = "https://randomuser.me/api/?results=3";
+// const API_URL = "https://randomuser.me/api/?results=3";
+const API_URL = 'http://localhost:8000/wp-json';
 
 const results = fetch(API_URL)
-  .then(x => {
-    console.info("x", x);
-    // Stream is converted to Promise after json() call
-    return x.json().then(y => {
-      const results = y.results;
-      console.info("results", results);
-      results.map(z => {
-        console.log("name:", z.name.first + " " + z.name.last);
-        return z.picture.medium;
-      });
+  .then(async (intialResponse) => {
+    const transformedResponse = await intialResponse.json();
+    const routes = transformedResponse.routes;
+    Object.keys(routes).map((route) => {
+      // REGEX to remove the initial /wp/v2/
+      // And anything after the subsequent "/"
+      console.log(route);
     });
   })
-  .catch(e => console.error(e));
+  .catch((e) => console.error(e));
 
-console.log("Here we go!!!", results);
+results.then(() => console.log('Here we go!!!', results));
