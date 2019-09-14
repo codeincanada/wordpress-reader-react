094469140faaabd2eb89e5faf1e9cf851c932fd8
Daniel Santos <daniel@brasileiro.ca>
2019-09-14 15:27:03 -0400

    Modifies URL for posts only

--- source/entry.js
+++ source/entry.js
@@ -1,14 +1,11 @@
 // const API_URL = "https://randomuser.me/api/?results=3";
-const API_URL = "http://localhost:8000/wp-json";
+const API_URL = "http://localhost:8000/wp-json/wp/v2/posts";
 
 const results = fetch(API_URL)
   .then(async intialResponse => {
     const transformedResponse = await intialResponse.json();
-    const routes = transformedResponse.routes;
-    Object.keys(routes).map(route => {
-      // REGEX to remove the initial /wp/v2/
-      // And anything after the subsequent "/"
-      console.log(route);
+    transformedResponse.map(entry => {
+      console.info(entry);
     });
   })
   .catch(e => console.error(e));
