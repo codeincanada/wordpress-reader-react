413dc3a63f6fd0b3bb0ec34aee9f3394951f0bde
Daniel Santos <daniel@brasileiro.ca>
2019-09-14 13:16:59 -0400

    Update eslint and prettier usage

--- source/entry.js
+++ source/entry.js
@@ -1,16 +1,16 @@
 // const API_URL = "https://randomuser.me/api/?results=3";
-const API_URL = 'http://localhost:8000/wp-json';
+const API_URL = "http://localhost:8000/wp-json";
 
 const results = fetch(API_URL)
-  .then(async (intialResponse) => {
+  .then(async intialResponse => {
     const transformedResponse = await intialResponse.json();
     const routes = transformedResponse.routes;
-    Object.keys(routes).map((route) => {
+    Object.keys(routes).map(route => {
       // REGEX to remove the initial /wp/v2/
       // And anything after the subsequent "/"
       console.log(route);
     });
   })
-  .catch((e) => console.error(e));
+  .catch(e => console.error(e));
 
-results.then(() => console.log('Here we go!!!', results));
+results.then(() => console.log("Here we go!!!", results));
