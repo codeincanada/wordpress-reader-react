408dde93b80a55d91eed3adf6f32ffbf270e4e33
Daniel Santos <daniel.santos@cst.org>
2019-09-18 18:19:31 -0400

    Posts are displayed upon clicking on button

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,90 +1,72 @@
-import React from "react";
-import styles from "./Post.module.scss";
+import React, { useState } from 'react';
+import styles from './Post.module.scss';
 
-// Create an Inline list shared component
-// const Categories = props => {
-//   const items = props.in.map((e, i) => <li key={i}>{e}</li>);
-//   const classes = "InlineList " + props.className;
-//   return <ul className={classes}>{items}</ul>;
-// };
-// const Tags = props => {
-//   const items = props.in.map((e, i) => <li key={i}>{e}</li>);
-//   const classes = "InlineList " + props.className;
+const Post = (props: any) => {
+    const [expandPost, setExpandPost] = useState(false);
+    const [showContent, setShowContent] = useState(false);
+    
+    // TODO: Emit an event to the parent to notify a post was selected
+    const post = props.in;
 
-//   return <ul className={classes}>{items}</ul>;
-// };
-
-const Post = (props:any) => {
-  // console.log("=D", props);
-
-  const post = props.in;
-  // const categories = props.categories;
-  // const tags = props.tags;
-
-  // TODO: move to an external lib
-  // const translatedCategories = post.categories.map(postCatId => {
-  //   if (categories) {
-  //     const found = categories.find(catId => {
-  //       if (postCatId === catId.id) {
-  //         return catId.name;
-  //       } else {
-  //         return null;
-  //       }
-  //     });
-  //     return found.name;
-  //   } else {
-  //     return null;
-  //   }
-  // });
-
-  // TODO: move to an external lib
-  // const translatedTags = post.tags.map(postTagId => {
-  //   if (tags) {
-  //     const found = tags.find(tagId => {
-  //       if (postTagId === tagId.id) {
-  //         return tagId.name;
-  //       } else {
-  //         return null;
-  //       }
-  //     });
-  //     return found.name;
-  //   } else {
-  //     return null;
-  //   }
-  // });
-
-  // if (translatedTags) console.info(translatedTags);
-
-  if (!post) {
-    return <h1>Post not loaded</h1>;
-  } else {
-    // console.info(post);
-    return (
-      <article className={styles.Root} key={post.id}>
-        <h2 className={styles.PostTitleContainer}>
-          <a href={post.link} className={styles.LinkWrapper} title={'Post ID = ' + post.id}>
-            {post.title.rendered}
-          </a>
-          {/* <span className={styles.PostID}>ID:{post.id}</span> */}
-        </h2>
-        <aside className={styles.PostFooter}>
-          {/* <div className="CatsAndTags"> */}
-          {/* <span className="firstRow">Published on {post.date}</span> */}
-          {/* <Categories in={translatedCategories} className="secondRow" />
-            <Tags in={translatedTags} className="thirdRow" /> */}
-          {/* </div> */}
-          <div className={styles.ButtonContainer}>
-            {/* TODO: Service Worker and Local Storage */}
-            <button id="save">Save offline</button>
-            {/* TODO: post.excerpt.rendered */}
-            <button id="expand">Expand</button>
-            {/* TODO: Local state and Class manipulation */}
-            <button id="hide">Hide</button>
-          </div>
-        </aside>
-      </article>
-    );
-  }
+    if (!post) {
+        return <h1>Post not loaded</h1>;
+    } else {
+        return (
+            <article className={styles.NewRoot}>
+                <main className={styles.Root} key={post.id}>
+                    <h2
+                        className={styles.PostTitleContainer}
+                        title={'Post ID = ' + post.id}
+                        onClick={() => setExpandPost(!expandPost)}
+                    >
+                        {/* <a href={post.link} className={styles.LinkWrapper}> */}
+                        {post.title.rendered}
+                        {/* </a> */}
+                    </h2>
+                    {expandPost && (
+                        <aside className={styles.PostFooter}>
+                            {expandPost && (
+                                <div className={styles.ButtonContainer}>
+                                    {/* TODO: Service Worker and Local Storage */}
+                                    <button id="save">
+                                        <span role="img" aria-label="Icon">
+                                            ⏰
+                                        </span>{' '}
+                                        Save offline
+                                    </button>
+                                    {/* TODO: post.excerpt.rendered */}
+                                    <button
+                                        id="expand"
+                                        onClick={() => setShowContent(!showContent)}
+                                    >
+                                        <span role="img" aria-label="Icon">
+                                            🎶
+                                        </span>
+                                        Read now
+                                    </button>
+                                    {/* TODO: Local state and Class manipulation */}
+                                    <button id="hide">
+                                        <span role="img" aria-label="Icon">
+                                            ✋
+                                        </span>
+                                        Delete
+                                    </button>
+                                </div>
+                            )}
+                        </aside>
+                    )}
+                </main>
+                {showContent && (
+                    <aside
+                        className={styles.Content}
+                        dangerouslySetInnerHTML={{
+                            __html: post.content.rendered,
+                        }}
+                    ></aside>
+                )}
+            </article>
+        );
+    }
 };
 
 export default Post;
--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,23 +1,24 @@
-import React, { useState } from "react";
-import Post from "./Post/Post";
-import GetCategories from "../libs/GetCategories";
-import GetTags from "../libs/GetTags";
-import Loader from "./Loader/Loader";
+import React, { useState } from 'react';
+import Post from './Post/Post';
+import GetCategories from '../libs/GetCategories';
+import GetTags from '../libs/GetTags';
+import Loader from './Loader/Loader';
 
-const Posts = (props:any) => {
-  const [categories, setCategories] = useState();
-  const [tags, setTags] = useState();
+const Posts = (props: any) => {
+    const [categories, setCategories] = useState();
+    const [tags, setTags] = useState();
 
-  GetTags.then(x => setTags(x));
-  GetCategories.then(x => setCategories(x));
+    // TODO: Capture an event from the child to be aware that a post was selected
+    GetTags.then(x => setTags(x));
+    GetCategories.then(x => setCategories(x));
 
-  if (!props.in) {
-    return <Loader />;
-  } else {
-    return props.in.map((e:any) => (
-      <Post in={e} tags={tags} categories={categories} key={e.id} />
-    ));
-  }
+    if (!props.in) {
+        return <Loader />;
+    } else {
+        return props.in.map((e: any) => (
+            <Post in={e} tags={tags} categories={categories} key={e.id} />
+        ));
+    }
 };
 
 export default Posts;
