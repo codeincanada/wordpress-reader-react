db8d79c8803b259e1e861dc73cfacf2648c99257
Daniel Santos <daniel.santos@cst.org>
2019-09-20 14:24:47 -0400

    Change API target to CSS tricks

--- src/libs/GetPosts.js
+++ src/libs/GetPosts.js
@@ -1,4 +1,5 @@
-const API_URL = "http://localhost:8000/wp-json/wp/v2/posts";
+// const API_URL = "http://localhost:8000/wp-json/wp/v2/posts";
+const API_URL = "https://css-tricks.com/wp-json/wp/v2/posts";
 
 const GetPosts = fetch(API_URL)
   .then(async intialResponse => {
